27dad204cdf10a7c9e5a008a85f4c76b7c947352
qa <x013362@gmail.com>
2021-02-26 15:41:23 +0200

    wip add influxdb

---
+++ env/heartbeat/heartbeat.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export USER=`whoami`
+export HOST=`hostname`
+
+env|sort
+
+function send_data(){
+    data="heartbeat,host=$HOST,user=$USER value=$((1 + RANDOM % 100)) $(date +%s000000001)"
+    echo $data
+    curl -sw '%{http_code} %{url_effective}\n' --request POST "${INFLUX_URL}/api/v2/write?org=${DOCKER_INFLUXDB_INIT_ORG}&bucket=${DOCKER_INFLUXDB_INIT_BUCKET}"  \
+         --header "Authorization: Token ${DOCKER_INFLUXDB_INIT_ADMIN_TOKEN}" \
+         --data-raw "$data"
+}
+
+for (( ; ; ))
+do
+   send_data
+   sleep 10
+done
